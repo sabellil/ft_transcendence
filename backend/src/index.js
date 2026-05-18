45abc50fc552777fe9d187918bc87cf140287cf3
sabellil <sabellil@student.42.fr>
2026-05-18 14:31:50 +0200

    Updated App.tsx to import React tools and start of App function to describe chat main page should display

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,8 +8,19 @@ app.get("/", (req, res) => {//route HTTP GET, / page principale du backend
 //request recue du client : contient URL body header user etc
 //response envoyee au client
 
+app.get("/api/test", (req, res) => {
+    res.json({
+        message: "Hello from backend"
+    });
+});
+
 app.listen(3000, () => {//demarre le serveur sur le port 3000, backend ecoute localhost:3000
     console.log("Server running on port 3000");//msg affiche dans le terminal quand le serveur demarre
 });
 
-//TODO connect frontend to backend
+/*
+TODO connect frontend to backend --> besoin de route API backend 
+Process: 
+- Frontend React demande a voir mon profil avec GET /api/users/me
+- Backend recoit la req, verifie le token JWT, cherche les infos en bdd et renvoie la resq. 
+*/
