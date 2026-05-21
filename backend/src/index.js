b66a643f66147f53fad7946b8ce1355417d9ffb0
sabellil <sabellil@student.42.fr>
2026-05-21 14:28:52 +0200

    Succesful migration from Express to Fastify, npm run dev works and http://localhost:3000/ http://localhost:3000/api/test both dhow adequate messages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,36 @@
-const express = require("express");//importation de la biblio Express, comme un include
-const cors = require("cors");
-const app = express();//serveur backend
-app.use(cors());
+const Fastify = require("fastify");//installation de la biblio, comme un include
 
-app.get("/", (req, res) => {//route HTTP GET, / page principale du backend
-    res.send("Backend running");//envoie reponse texte au navigateur
-});//ferme la route GET
-//request recue du client : contient URL body header user etc
-//response envoyee au client
-
-app.get("/api/test", (req, res) => {
-    res.json({
-        message: "Hello from backend"
-    });
+const app = Fastify({
+    logger: true
 });
 
-app.listen(3000, () => {//demarre le serveur sur le port 3000, backend ecoute localhost:3000
-    console.log("Server running on port 3000");//msg affiche dans le terminal quand le serveur demarre
+app.register(require("@fastify/cors"));
+
+app.get("/", async (request, reply) => {//route HTTP GET, / page principale du backend
+
+    return "Backend running";
+
+});//ferme la route GET
+//request recue du client : contient URL body header user etc
+//reply envoyee au client
+
+app.get("/api/test", async (request, reply) => {
+
+    return {
+        message: "Hello from backend"
+    };
+
+});
+
+//autorisation des connexions internes, utiles plus tard pour Docker
+app.listen({
+    port: 3000,
+    host: "0.0.0.0"
 });
 
 /*
 TODO connect frontend to backend --> besoin de route API backend 
 Process: 
 - Frontend React demande a voir mon profil avec GET /api/users/me
-- Backend recoit la req, verifie le token JWT, cherche les infos en bdd et renvoie la resq. 
+- Backend Fastify recoit la req, verifie le token JWT, cherche les infos en bdd et renvoie la resq. 
 */
