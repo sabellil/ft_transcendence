7b8ef08b660cbfeca2b1c237d81ac44c9a2e3c43
sabellil <sabellil@student.42.fr>
2026-05-25 15:23:10 +0200

    Fixed Prisma 7 config postgres adapter for register endpoint

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,7 +1,5 @@
 import bcrypt from "bcrypt";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "../lib/prisma.js";
 
 export async function registerUser(
 	email: string,
