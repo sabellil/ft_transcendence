c0f3ed78f3709476f64feed9378c8ca9ce6fa0b6
sabellil <sabellil@student.42.fr>
2026-05-26 12:44:28 +0200

    Auth : JWT protection through app.addHook for orgs and users

--- backend/src/routes/orgs.ts
+++ backend/src/routes/orgs.ts
@@ -1,5 +1,9 @@
+import { 
+    authMiddleware 
+} from "../middleware/authMiddleware.js";
 async function orgRoutes(app, options)
 {
+    app.addHook("preHandler", authMiddleware);//middleware pour auth avant chaque route, sinon error 401 unauthorized
     app.get("/test", async (request, reply) =>
     {
         return {
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,7 +1,13 @@
+
+import {
+    authMiddleware 
+} from "../middleware/authMiddleware.js";
+
 import type { FastifyInstance } from "fastify";
 
 async function usersRoutes(app: FastifyInstance, options)
 {
+    app.addHook("preHandler", authMiddleware);//middleware pour auth avant chaque route, sinon error 401 unauthorized
     app.get("/test", async (request, reply) =>
     {
         return {
