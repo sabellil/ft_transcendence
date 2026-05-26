5df825ccc17d096b0a0bb62a029312b2f7bda93b
sabellil <sabellil@student.42.fr>
2026-05-26 12:43:53 +0200

    Auth : Added proper JWT  protection app.addHook to check with middleware the token for all current routes friends/orgs/users

--- backend/src/routes/friends.ts
+++ backend/src/routes/friends.ts
@@ -4,6 +4,7 @@ import {
 
 async function friendsRoutes(app, options)//option eventuelles config ou plugins pour plus tard?
 {
+    app.addHook("preHandler", authMiddleware);//middleware pour auth avant chaque route, sinon error 401 unauthorized
     app.get("/test", async (request, reply) =>
     {
         return {
