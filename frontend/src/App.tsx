ccc962a80597b03db324788494082e74dbf2f0e8
sabellil <sabellil@student.42.fr>
2026-05-28 14:54:37 +0200

    Trying to have features displayed with frontend. Completed main App function with the login function, need to code the HTML part to actually see something

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,49 @@
 import { useEffect, useState } from "react";
 
 //composant principal React qui decrit ce que la page doit afficher
-function App(){
-  const [message, setMessage] = useState("");//message = "hello from backend" du package.json, setMessage pour modifier message, mais ici vide
-  useEffect(() => {//exec du code autom. qund la page charge
-    fetch("http://localhost:3000/api/test")//frontend appelle le backend  en enovyant une requete HTTP vers local...
-      .then((res) => res.json())//recupere la res (reponse http brute) et la convertit en Javascript
-      .then((data) => {
-          setMessage(data.message);
-      });
-  }, []);//[] --> n'effectuer useEffect qu'une fois au chargement de la page
-  return (
-    <div>
-      <h1>{message}</h1>
-    </div>
-  );
+function App()
+{
+  //declaration de 3 variables d'etat pour stocker les donnees du formulaire et le token de connexion
+	const [email, setEmail] =
+		useState("");
+
+	const [password, setPassword] =
+		useState("");
+
+	const [token, setToken] =
+		useState("");
+  
+  //fonction qui s'execute au clic du bouton de login
+	async function login()
+	{
+		const response =
+			await fetch(//envoie une requete HTTP au backend pour se connecter
+				"http://localhost:3000/api/auth/login",
+				{
+					method: "POST",
+					headers:
+					{
+						"Content-Type":
+						"application/json"
+					},
+					body: JSON.stringify({//transforme les donnees du formulaire en JSON pour les envoyer au backend
+						email,
+						password
+					})
+				}
+			);
+
+		const data =
+			await response.json();
+    //stocke le token de connexion recu du backend dans la variable d'etat token
+		setToken(data.token);
+	}
+
+	return (
+		//Need to ecrire du html React ici ! affichage de la page, formulaire de login
+	);
 }
+
 //autoriser les autres fichiers a utilsier cette fonction 
 export default App;
 /*
