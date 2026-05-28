c40d6999f8eefcf6c20af53210e16d498276ef8c
sabellil <sabellil@student.42.fr>
2026-05-28 12:57:29 +0200

    Avatar upload OK, need to add .png

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,9 +1,11 @@
-
 import {
     authMiddleware 
 } from "../middleware/authMiddleware.js";
 
 import type { FastifyInstance } from "fastify";
+
+import fs from "fs";
+import path from "path";
 
 import {
     getMe, 
@@ -31,9 +33,7 @@ async function usersRoutes(app: FastifyInstance, options)
 			return await getMe(
 				user.id
 			);
-		}
-	);
-
+		});
 	app.put(
 		"/me",
 		async (request, reply) =>
@@ -54,10 +54,46 @@ async function usersRoutes(app: FastifyInstance, options)
 				user.id,
 				username
 			);
+		});
+	app.post(
+	"/avatar",
+	async (request, reply) =>
+	{
+		const data =
+			await request.file();
+
+		if (!data)
+		{
+			return reply.status(400).send({
+				error:"No file"
+			});
 		}
-	);
 
+		const user =
+			request.user;
 
+		const fileName =
+			`${user.id}-${data.filename}`;
+
+		const filePath =
+			path.join(
+				"uploads/avatars",
+				fileName
+			);
+
+		const buffer =
+			await data.toBuffer();
+
+		fs.writeFileSync(
+			filePath,
+			buffer
+		);
+
+		return {
+			message:"Avatar uploaded",
+			path:filePath
+		};
+	});
 }
 
 export default usersRoutes;
