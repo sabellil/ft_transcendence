7d94cbf856c64457475262cda0b50820cd86982d
sabellil <sabellil@student.42.fr>
2026-06-25 16:21:39 +0200

    Wrote a plan for realtime ts

---
+++ backend/src/realtime.ts
@@ -0,0 +1,9 @@
+//initialiser socket.IO
+//recuperer user depuis cookie JWT
+//marquer Online le user
+//Prevenir les amis que le user est Online
+//Gerer disconnect GrAceFulLy
+//marquer Offline le user
+//Prevenir les amis que le user est Offline
+
+
